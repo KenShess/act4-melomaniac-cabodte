20993c9e9afee556e8affd7ef52da74394198e01
CabodteKenneth <kennethcabodte@gmail.com>
2022-05-15 15:37:14 +0800

    Song 4 added

---
+++ song4.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Melomaniac Activity</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+      
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
+            <div class="container px-4">
+                <a class="navbar-brand" href="#page-top"> Lyre Bird </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link text-light" href="song1.php">Song 1</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song2.php">Song 2</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song3.php">Song 3</a></li>
+                         <li class="nav-item"><a class="nav-link text-light" href="song4.php">Song 4</a></li>
+                          <li class="nav-item"><a class="nav-link text-light" href="song5.php">Song 5</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+        <!-- About section-->
+        <section id="songs">
+            <div class="container px-4 ">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-12 text-center">
+                        <h2 class="text-primary">Teenage Mona Lisa</h2>
+                        <p class="lead">Alfie Castley</p>
+
+                        <?php
+
+                        echo "<p> Feel like I'm talking to a teenage Mona LisaSo beautiful, yet so unsatisfied</p>";
+                        echo "<p> So beautiful, yet so unsatisfied</p>";
+                        echo "<p> And when I take a trip to Paris out to meet her</p>";
+                        echo "<p> If she don't care, at least I'll know I tried</p>";
+                        echo "<br/>";
+
+                        echo "<p> Think I'm getting tired of these games now</p>";
+                        echo "<p> At least the way that they play out</p>";
+                        echo "<p> When your mind is on the wrong side</p>";
+                        echo "<p> When I'm all alone with my thoughts out</p>";
+                        echo "<p> And the gallery walls how</p>";
+                        echo "<p> My heart will start to realize</p>";
+                        echo "<br/>";
+
+                        echo "<p> That I don't really love you</p>";
+                        echo "<p> Do you think that I know you?</p>";
+                        echo "<br/>";
+
+                        echo "<p> Feel like I'm talking to a teenage Mona Lisa</p>";
+                        echo "<p> So beautiful, yet so unsatisfied</p>";
+                        echo "<p> And when I take a trip to Paris out to meet her</p>";
+                        echo "<p> If she don't care, at least I'll know I tried</p>";
+                        echo "<br/>";
+
+                        echo "<p> Standing at the top of a mountain</p>";
+                        echo "<p> And you don't hear me shouting</p>";
+                        echo "<p> And I've called around a thousand times</p>";
+                        echo "<p> Silent and you don't even listen</p>";
+                        echo "<p> Was it you that I'm missing?</p>";
+                        echo "<p> Maybe I don't wanna realize</p>";
+                        echo "<br/>";
+
+                        echo "<p> That I don't really love you</p>";
+                        echo "<p> Do you think that I know you?</p>";
+                        echo "<br/>";
+
+                        echo "<p> Feel like I'm talking to a teenage Mona Lisa</p>";
+                        echo "<p> So beautiful, yet so unsatisfied</p>";
+                        echo "<p> And when I take a trip to Paris out to meet her</p>";
+                        echo "<p> If she don't care, at least I'll know I tried</p>";
+                        echo "<br/>";
+
+                        echo "<p> Feel like I'm talking to a teenage Mona Lisa</p>";
+                        echo "<p> So beautiful, yet so unsatisfied</p>";
+                        echo "<p> And when I take a trip to Paris out to meet her</p>";
+                        echo "<p> If she don't care, at least I'll know I tried</p>";
+                        echo "<br/>";
+
+                        echo "<p> Feel like I'm talking to a teenage Mona Lisa</p>";
+                        echo "<p> So beautiful, yet so unsatisfied</p>";
+                        echo "<br/>";
+
+                        ?>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer class="py-3 bg-danger">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Kenneth Cabodte</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
