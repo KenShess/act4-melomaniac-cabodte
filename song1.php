6cc549c871930c408144a2f5b18ca23d641374b0
CabodteKenneth <kennethcabodte@gmail.com>
2022-05-15 14:45:43 +0800

    Song 1 added

---
+++ song1.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Melomaniac Activity</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+      
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
+            <div class="container px-4">
+                <a class="navbar-brand" href="#page-top"> Lyre Bird </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link text-light" href="song1.php">Song 1</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song2.php">Song 2</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song3.php">Song 3</a></li>
+                         <li class="nav-item"><a class="nav-link text-light" href="song4.php">Song 4</a></li>
+                          <li class="nav-item"><a class="nav-link text-light" href="song5.php">Song 5</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+        <!-- About section-->
+        <section id="songs">
+            <div class="container px-4 ">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-12 text-center">
+                        <h2 class="text-primary">Be Somebody</h2>
+                        <p class="lead">Phil Good</p>
+
+                        <?php
+
+                        echo "<p> Are you sure you know what you're doing?</p>";
+                        echo "<br/>";
+
+                        echo "<p> Another sleepless night, overthinkin' my life</p>";
+                        echo "<p> Things I shoulda done better, and I said I would never</p>";
+                        echo "<p> Let it pass me by but, in the blink of an eye</p>";
+                        echo "<p> Well I'm hittin' 26 now, and I'm feelin' kinda left out</p>";
+                        echo "<br/>";
+
+                        echo "<p> Can't help but compare, all my friends already there</p>";
+                        echo "<p> I can say that it's not fair but maybe somehow it's my fault</p>";
+                        echo "<br/>";
+
+                        echo "<p> I had some money but I spent it all</p>";
+                        echo "<p> Leaned into love but I forgot to fall</p>";
+                        echo "<p> Lost track of time, so irresponsible</p>";
+                        echo "<p> Now I gotta be somebody, gotta be somebody, oh no</p>";
+                        echo "<br/>";
+
+                        echo "<p> Gotta be somebody</p>";
+                        echo "<p> Gotta be somebody, oh no</p>";
+                        echo "<p> Gotta be somebody, yeah</p>";
+                        echo "<p> Gotta be somebody</p>";
+                        echo "<br/>";
+
+                        echo "<p> Another wasted day, another torn out page</p>";
+                        echo "<p> Feels like I hit my peak when I was 17 and</p>";
+                        echo "<p> Everyday I'm afraid I'm not even in the race and</p>";
+                        echo "<p> And I'm feelin' kinda left out</p>";
+                        echo "<br/>";
+
+                        echo "<p> But I can't help but compare, all my friends already there</p>";
+                        echo "<p> I can say that it's not fair but maybe somehow it's my fault, yeah</p>";
+                        echo "<br/>";
+
+                        echo "<p> I had some money but I spent it all</p>";
+                        echo "<p> Leaned into love but I forgot to fall</p>";
+                        echo "<p> Lost track of time, so irresponsible</p>";
+                        echo "<p> Now I gotta be somebody, gotta be somebody, oh no</p>";
+                        echo "<br/>";
+
+                        echo "<p> Gotta be somebody</p>";
+                        echo "<p> Gotta be somebody, oh no</p>";
+                        echo "<p> Gotta be somebody, yeah</p>";
+                        echo "<p> Gotta be somebody</p>";
+                        echo "<br/>";
+
+                        echo "<p> Everyday I'm afraid I'm not even in the race and</p>";
+                        echo "<p> And I'm feelin' kinda left out, yeah</p>";
+                        echo "<p> Everyday I'm afraid I'm not even in the race and</p>";
+                        echo "<p> And I'm feelin' kinda left out, now I gotta be somebody</p>";
+                        echo "<br/>";
+
+                        echo "<p> Be somebody</p>";
+                        echo "<p> Gotta be somebody, oh-oh</p>";
+                        echo "<p> Gotta be somebody</p>";
+                        echo "<p> Gotta be somebody</p>";
+                        echo "<br/>";
+                        ?>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer class="py-3 bg-danger">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Kenneth Cabodte</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
