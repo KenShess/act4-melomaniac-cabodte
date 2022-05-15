f67cda40e3a8d8b70e3e6bd158219a5096d2a919
CabodteKenneth <kennethcabodte@gmail.com>
2022-05-15 15:04:35 +0800

    Song 2 added

---
+++ song2.php
@@ -0,0 +1,172 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Melomaniac Activity</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+      
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
+            <div class="container px-4">
+                <a class="navbar-brand" href="#page-top"> Lyre Bird </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link text-light" href="song1.php">Song 1</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song2.php">Song 2</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song3.php">Song 3</a></li>
+                         <li class="nav-item"><a class="nav-link text-light" href="song4.php">Song 4</a></li>
+                          <li class="nav-item"><a class="nav-link text-light" href="song5.php">Song 5</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+        <!-- About section-->
+        <section id="songs">
+            <div class="container px-4 ">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-12 text-center">
+                        <h2 class="text-primary">Payphone</h2>
+                        <p class="lead">Maroon 5 (feat. Wiz Khalifa)</p>
+
+                        <?php
+
+                        echo "<p> I'm at a payphone trying to call home</p>";
+                        echo "<p> All of my change I spent on you</p>";
+                        echo "<p> Where have the times gone? Baby, it's all wrong</p>";
+                        echo "<p> Where are the plans we made for two?</p>";
+                        echo "<br/>";
+
+                        echo "<p> Yeah, I, I know it's hard to remember</p>";
+                        echo "<p> The people we used to be...</p>";
+                        echo "<p> It's even harder to picture,</p>";
+                        echo "<p> That you're not here next to me.</p>";
+                        echo "<br/>";
+
+                        echo "<p> You say it's too late to make it,</p>";
+                        echo "<p> But is it too late to try?</p>";
+                        echo "<p> And in our time that you wasted</p>";
+                        echo "<p> All of our bridges burned down</p>";
+                        echo "<br/>";
+
+                        echo "<p> I've wasted my nights,</p>";
+                        echo "<p> You turned out the lights</p>";
+                        echo "<p> Now I'm paralyzed.</p>";
+                        echo "<p> Still stuck in that time</p>";
+                        echo "<p> When we called it love</p>";
+                        echo "<p> But even the sun sets in paradise</p>";
+                        echo "<br/>";
+
+                        echo "<p> I'm at a payphone trying to call home</p>";
+                        echo "<p> All of my change I spent on you</p>";
+                        echo "<p> Where have the times gone? Baby, it's all wrong</p>";
+                        echo "<p> Where are the plans we made for two?</p>";
+                        echo "<br/>";
+
+                        echo "<p> If 'Happy Ever After' did exist,</p>";
+                        echo "<p> I would still be holding you like this</p>";
+                        echo "<p> All those fairy tales are full of shit</p>";
+                        echo "<p> All those fairy tales are full of it.</p>";
+                        echo "<p> One more fucking love song, I'll be sick.</p>";
+                        echo "<p> One more stupid love song, I'll be sick</p>";
+                        echo "<br/>";
+
+                        echo "<p> Oh, you turned your back on tomorrow</p>";
+                        echo "<p> 'Cause you forgot yesterday.</p>";
+                        echo "<p> I gave you my love to borrow,</p>";
+                        echo "<p> But you just gave it away.</p>";
+                        echo "<br/>";
+
+                        echo "<p> You can't expect me to be fine,</p>";
+                        echo "<p> I don't expect you to care</p>";
+                        echo "<p> I know I've said it before</p>";
+                        echo "<p> But all of our bridges burned down.</p>";
+                        echo "<br/>";
+
+                        echo "<p> I've wasted my nights,</p>";
+                        echo "<p> You turned out the lights</p>";
+                        echo "<p> Now I'm paralyzed.</p>";
+                        echo "<p> Still stuck in that time</p>";
+                        echo "<p> When we called it love</p>";
+                        echo "<p> But even the sun sets in paradise</p>";
+                        echo "<br/>";
+
+                        echo "<p> I'm at a payphone trying to call home</p>";
+                        echo "<p> All of my change I spent on you</p>";
+                        echo "<p> Where have the times gone? Baby, it's all wrong</p>";
+                        echo "<p> Where are the plans we made for two?</p>";
+                        echo "<br/>";
+
+                        echo "<p> If 'Happy Ever After' did exist,</p>";
+                        echo "<p> I would still be holding you like this</p>";
+                        echo "<p> All those fairy tales are full of shit</p>";
+                        echo "<p> All those fairy tales are full of it.</p>";
+                        echo "<p> One more fucking love song, I'll be sick.</p>";
+                        echo "<p> One more stupid love song, I'll be sick</p>";
+                        echo "<br/>";
+
+                        echo "<p> Yeah, yeah, now baby don't hang up,</p>";
+                        echo "<p> So I can tell you what you need to know,</p>";
+                        echo "<p> Baby I'm begging you just please don't go,</p>";
+                        echo "<p> So I can tell you what you need to know</p>";
+                        echo "<br/>";
+
+                        echo "<p> Man, fuck that shit</p>";
+                        echo "<p> I'll be out spending all this money</p>";
+                        echo "<p> While you're sitting 'round wondering</p>";
+                        echo "<p> Why it wasn't you who came up from nothing,</p>";
+                        echo "<p> Made it from the bottom</p>";
+                        echo "<p> Now when you see me I'm stunting,</p>";
+                        echo "<p> And all of my cars start with a push of a button</p>";
+                        echo "<p> Telling me I changed since I blew up</p>";
+                        echo "<p> Or whatever you call it,</p>";
+                        echo "<p> Switched the number to my phone</p>";
+                        echo "<p> So you never could call it,</p>";
+                        echo "<p> Don't need my name on my shirt,</p>";
+                        echo "<p> You can tell that I'm balling.</p>";
+                        echo "<p> Swish, what a shame could have got picked</p>";
+                        echo "<p> Had a really good game but you missed your last shot</p>";
+                        echo "<p> So you talk about who you see at the top</p>";
+                        echo "<p> Or what you could have saw but sad to say it's over for.</p>";
+                        echo "<p> Phantom pulled up valet open doors</p>";
+                        echo "<p> Wished I'd go away, got what you was looking for</p>";
+                        echo "<p> Now it's me who they want, so you can go and take</p>";
+                        echo "<p> That little piece of shit with you.</p>";
+                        echo "<br/>";
+
+                        echo "<p> I'm at a payphone trying to call home</p>";
+                        echo "<p> All of my change I spent on you</p>";
+                        echo "<p> Where have the times gone? Baby, it's all wrong</p>";
+                        echo "<p> Where are the plans we made for two?</p>";
+                        echo "<br/>";
+
+                        echo "<p> If 'Happy Ever After' did exist,</p>";
+                        echo "<p> I would still be holding you like this</p>";
+                        echo "<p> All those fairy tales are full of shit</p>";
+                        echo "<p> All those fairy tales are full of it.</p>";
+                        echo "<p> One more fucking love song, I'll be sick.</p>";
+                        echo "<p> One more stupid love song, I'll be sick</p>";
+                        echo "<br/>";
+                        
+                        ?>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer class="py-3 bg-danger">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Kenneth Cabodte</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
