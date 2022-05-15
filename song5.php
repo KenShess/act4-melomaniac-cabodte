e047bf8bcb25af59f99fb5a3de2c478e2ccf2ff9
CabodteKenneth <kennethcabodte@gmail.com>
2022-05-15 15:49:11 +0800

    Song 5 added

---
+++ song5.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Melomaniac Activity</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+      
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
+            <div class="container px-4">
+                <a class="navbar-brand" href="#page-top"> Lyre Bird </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link text-light" href="song1.php">Song 1</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song2.php">Song 2</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song3.php">Song 3</a></li>
+                         <li class="nav-item"><a class="nav-link text-light" href="song4.php">Song 4</a></li>
+                          <li class="nav-item"><a class="nav-link text-light" href="song5.php">Song 5</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+        <!-- About section-->
+        <section id="songs">
+            <div class="container px-4 ">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-12 text-center">
+                        <h2 class="text-primary">Marry Me</h2>
+                        <p class="lead">Jason Derulo</p>
+
+                        <?php
+
+                        echo "<p> A hundred and five is the number that comes to my head</p>";
+                        echo "<p> When I think of all the years I wanna be with you</p>";
+                        echo "<p> Wake up every morning with you in my bed</p>";
+                        echo "<p> That's precisely what I plan to do</p>";
+                        echo "<br/>";
+
+                        echo "<p> And you know one of these days when I get my money right</p>";
+                        echo "<p> Buy you everything and show you all the finer things in life</p>";
+                        echo "<p> We'll forever be in love, so there ain't no need to rush</p>";
+                        echo "<p> But one day I won't be able to ask you loud enoug</p>";
+                        echo "<br/>";      
+
+                        echo "<p> I'll say, 'Will you marry me?'</p>";
+                        echo "<p> I swear that I will mean it</p>";
+                        echo "<p> I'll say, 'Will you marry me?'</p>";
+                        echo "<br/>";                  
+
+                        echo "<p> Singing</p>";
+                        echo "<p> Ooh whoa ooh oh</p>";
+                        echo "<p> Ooh whoa ooh oh</p>";
+                        echo "<p> Ooh whoa ooh oh</p>";
+                        echo "<p> Oh, yeah</p>";
+                        echo "<br/>";
+
+                        echo "<p> How many girls in the world can make me feel like this?</p>";
+                        echo "<p> Baby I don't ever plan to find out</p>";
+                        echo "<p> The more I look, the more I find the reasons why</p>";
+                        echo "<p> You're the love of my life</p>";
+                        echo "<br/>";
+
+                        echo "<p> You know one of these days when I get my money right</p>";
+                        echo "<p> Buy you everything and show you all the finer things in life</p>";
+                        echo "<p> We'll forever be in love, so there ain't no need to rush</p>";
+                        echo "<p> But one day I won't be able to ask you loud enough</p>";
+                        echo "<br/>";
+
+                        echo "<p> I'll say, 'Will you marry me?'</p>";
+                        echo "<p> I swear that I will mean it</p>";
+                        echo "<p> I'll say, 'Will you marry me?'</p>";
+                        echo "<br/>";
+
+                        echo "<p> And if I lost everything</p>";
+                        echo "<p> In my heart it means nothing</p>";
+                        echo "<p> 'Cause I have you,</p>";
+                        echo "<p> Girl, I have you</p>";
+                        echo "<p> To get right down on bended knee</p>";
+                        echo "<p> Nothing else would ever be better, better</p>";
+                        echo "<p> That day when...</p>";
+                        echo "<br/>";
+
+                        echo "<p> I'll say, 'Will you marry me?'</p>";
+                        echo "<p> (I'll get down on one knee)/p>";
+                        echo "<p> I swear that I will mean it</p>";
+                        echo "<p> I'll say, 'Will you marry me?'</p>";
+                        echo "<br/>";
+
+                        echo "<p> Got me singing</p>";
+                        echo "<p> Ooh whoa ooh oh</p>";
+                        echo "<p> Got me singing</p>";
+                        echo "<p> Ooh whoa ooh oh</p>";
+                        echo "<p> Ooh whoa ooh oh</p>";
+                        echo "<p> Would you marry me, baby?</p>";
+                        echo "<p> Ooh whoa ooh oh</p>";
+                        echo "<p> Ooh whoa ooh oh</p>";
+                        echo "<p> Ooh whoa ooh oh</p>";
+                        echo "<br/>";
+
+                        echo "<p> A hundred and five is the number that comes to my head</p>";
+                        echo "<p> When I think of all the years I wanna be with you</p>";
+                        echo "<p> Wake up every morning with you in my bed</p>";
+                        echo "<p> That's precisely what I plan to do, yeah</p>";
+                        echo "<br/>";
+                        ?>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer class="py-3 bg-danger">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Kenneth Cabodte</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
