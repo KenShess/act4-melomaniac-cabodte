20cf9827047d39f1577b151e7ec13f81d2f4f3ca
CabodteKenneth <kennethcabodte@gmail.com>
2022-05-15 15:26:15 +0800

    Song 3 added

---
+++ song3.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Melomaniac Activity</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+      
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
+            <div class="container px-4">
+                <a class="navbar-brand" href="#page-top"> Lyre Bird </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link text-light" href="song1.php">Song 1</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song2.php">Song 2</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song3.php">Song 3</a></li>
+                         <li class="nav-item"><a class="nav-link text-light" href="song4.php">Song 4</a></li>
+                          <li class="nav-item"><a class="nav-link text-light" href="song5.php">Song 5</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+        <!-- About section-->
+        <section id="songs">
+            <div class="container px-4 ">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-12 text-center">
+                        <h2 class="text-primary">Sun and Moon</h2>
+                        <p class="lead">Anees</p>
+
+                        <?php
+
+                        echo "<p> Yeah, yeah</p>";
+                        echo "<br/>";
+
+                        echo "<p> Baby, baby, you're my sun and moon</p>";
+                        echo "<p> Girl, you're everything between</p>";
+                        echo "<p> A lot of pretty faces could waste my time</p>";
+                        echo "<p> But you're my dream girl</p>";
+                        echo "<p> You make flowers bloom</p>";
+                        echo "<p> Girl, you make the stars collide</p>";
+                        echo "<p> And I don't know what I did</p>";
+                        echo "<p> To get lucky like this but it sure feels fine</p>";
+                        echo "<br/>";
+
+                        echo "<p> 'Cause you give me love and affection</p>";
+                        echo "<p> I give you all my protection</p>";
+                        echo "<p> I'm tryna stay by your side till the day I die</p>";
+                        echo "<p> And you give me all of your love</p>";
+                        echo "<p> And I give you all of my trust</p>";
+                        echo "<p> I know you'll never betray me and leave my side, oh my</p>";
+                        echo "<br/>";
+
+                        echo "<p> Baby, baby, you're my sun and moon</p>";
+                        echo "<p> Girl, you're everything between</p>";
+                        echo "<p> A lot of pretty faces could waste my time</p>";
+                        echo "<p> But you're my dream girl</p>";
+                        echo "<p> You make flowers bloom</p>";
+                        echo "<p> (Girl, you make the flowers bloom)</p>";
+                        echo "<p> Girl, you make the stars collide</p>";
+                        echo "<p> (Don't you know you make the stars collide?)</p>";
+                        echo "<p> And I don't know what I did</p>";
+                        echo "<p> To get lucky like this but it sure feels fine</p>";
+                        echo "<br/>";
+
+                        echo "<p> 'Cause I see your face when I'm sleepin'</p>";
+                        echo "<p> I wake up grateful to breathe in</p>";
+                        echo "<p> The air you float through, I wrote you a lullaby</p>";
+                        echo "<p> A song to sing on your pillow</p>";
+                        echo "<p> Swift as a swing on a willow</p>";
+                        echo "<p> My sweetest words I'm the birds in your perfect sky, oh my</p>";
+                        echo "<br/>";
+
+                        echo "<p> Baby, baby, you're my sun and moon</p>";
+                        echo "<p> Girl, you're everything between</p>";
+                        echo "<p> A lot of pretty faces could waste my time</p>";
+                        echo "<p> But you're my dream girl (Girl)</p>";
+                        echo "<p> You make flowers bloom</p>";
+                        echo "<p> (Girl, you make the flowers bloom)</p>";
+                        echo "<p> Girl, you make the stars collide</p>";
+                        echo "<p> (Don't you know you make the stars collide?)</p>";
+                        echo "<p> And I don't know what I did</p>";
+                        echo "<p> To get lucky like this but it sure feels fine</p>";
+                        echo "<p> Baby, baby, you're my sun and moon</p>";
+                        echo "<p> Girl, you're everything between</p>";
+                        echo "<p> A lot of pretty faces could waste my time</p>";
+                        echo "<p> But you're my dream girl</p>";
+                        echo "<p> You make flowers bloom</p>";
+                        echo "<p> (Girl, you make the flowers bloom)</p>";
+                        echo "<p> Girl, you make the stars collide</p>";
+                        echo "<p> (Don't you know you make the stars collide?)</p>";
+                        echo "<p> And I don't know what I did</p>";
+                        echo "<p> To get lucky like this but it sure feels fine, oh my, oh my</p>";
+                        echo "<br/>";
+
+                        echo "<p> Oh my, oh my, oh my</p>";
+                        echo "<p> Yeah, oh my, oh my</p>";
+                        echo "<p> Baby, baby, you're mine</p>";
+                        echo "<br/>";
+                        
+                        ?>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer class="py-3 bg-danger">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Kenneth Cabodte</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
